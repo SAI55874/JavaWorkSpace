61094bf8c6a4bf6fb7274dc0f2e1af00c02b66cd
SAI <89627722+SAI55874@users.noreply.github.com>
2022-11-22 19:09:50 +0530

    Add files via upload

---
+++ jdbcDemowithexl/src/com/htc/daodemo/LogUtil/LogUtil.java
@@ -0,0 +1,23 @@
+package com.htc.daodemo.LogUtil;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LogUtil {
+	static Logger logger;
+	public static Logger getLog(String name) throws SecurityException, IOException
+	{
+		logger=Logger.getLogger(name);
+		FileHandler fh=new FileHandler("D:\\Employeelog.log", true);
+		fh.setLevel(Level.INFO);
+		fh.setFormatter(new SimpleFormatter());
+		logger.addHandler(fh);
+		
+		return logger;
+		
+	}
+
+}
